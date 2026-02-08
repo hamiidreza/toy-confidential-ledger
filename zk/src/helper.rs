1399b9aa43e25066a9f944fcc8893691e0679f79
Hamidreza <hamid.khoshakhlagh@partisia.com>
2026-02-08 21:09:01 +0100

    add helpers for Merlin transcript

---
+++ zk/src/helper.rs
@@ -0,0 +1,18 @@
+use ark_bn254::G1Projective as C;
+use ark_ec::CurveGroup;
+use ark_serialize::CanonicalSerialize;
+use merlin::Transcript;
+use ark_bn254::Fr;
+
+
+pub fn transcript_append_point(transcript: &mut Transcript, label: &'static [u8], p: &C) {
+    let mut buf = Vec::new();
+    p.into_affine().serialize_compressed(&mut buf).unwrap();
+    transcript.append_message(label, &buf);
+}
+
+pub fn transcript_append_element(transcript: &mut Transcript, label: &'static [u8], e: &Fr) {
+    let mut buf = Vec::new();
+    e.serialize_compressed(&mut buf).unwrap();
+    transcript.append_message(label, &buf);
+}
