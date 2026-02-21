4cb2660453d8fddb76e782a159c49ad31803e779
Hamidreza <hamid.khoshakhlagh@partisia.com>
2026-02-21 23:36:20 +0100

    add prove_transfer

--- zk/src/lib.rs
+++ zk/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod helper;
 pub mod inner_product;
 pub mod pedersen;
+pub mod prove_transfer;
 pub mod range_proof;
 pub mod verifiy_transfer;
---
+++ zk/src/prove_transfer.rs
@@ -0,0 +1,41 @@
+use crate::helper::*;
+use crate::pedersen::*;
+use crate::range_proof::{RangeProof, prove_less_than_or_equal};
+use ark_bn254::{Fr, G1Projective as C};
+use merlin::Transcript;
+use rand::thread_rng;
+
+pub fn create_transfer_proof(
+    v_transfer: u64,
+    r_transfer: Fr,
+    v_balance: u64,
+    r_balance: Fr,
+    gens: &Vec<(C, C)>,
+    key: &CommitmentKey,
+    n: u8,
+) -> (C, RangeProof) {
+    assert!(v_transfer <= v_balance, "Transfer exceeds balance");
+
+    // Construct C_v
+    let cm_v = C::from(key.g) * Fr::from(v_transfer) + C::from(key.h) * r_transfer;
+
+    let mut transcript = Transcript::new(b"ConfidentialLedger");
+    transcript_append_point(&mut transcript, b"value_commitment", &cm_v);
+
+    // Generate proof
+    let mut rng = thread_rng();
+    let proof = prove_less_than_or_equal(
+        &mut transcript,
+        &mut rng,
+        n,
+        v_transfer,
+        v_balance,
+        gens,
+        key,
+        &r_transfer,
+        &r_balance,
+    )
+    .expect("Failed to generate the range proof");
+
+    (cm_v, proof)
+}
