17e19402b699ebc32960706bcf8630ef4455bf56
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2026-03-30 21:50:59 +0200

    feat: initialize frontend with Next.js, TypeScript, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created postcss.config.mjs for Tailwind CSS integration
    - Added tsconfig.json for TypeScript configuration

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Unbounded, Montserrat } from "next/font/google";
+import "./globals.css";
+
+const headingFont = Unbounded({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-heading",
+});
+
+const bodyFont = Montserrat({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+  variable: "--font-body",
+});
+
+export const metadata: Metadata = {
+  title: "anonymous payment",
+  description: "anonymous payment dApp.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={`${headingFont.variable} ${bodyFont.variable}`}
+      suppressHydrationWarning
+    >
+      <body className="font-sans antialiased">
+        {/* Use the wrapper instead of the raw Navbar */}
+
+        {children}
+      </body>
+    </html>
+  );
+}
